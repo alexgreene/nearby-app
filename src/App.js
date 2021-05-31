879ac377f1ba90dc14483583c1de7cad89657a40
Alex Greene <alexgrn7@gmail.com>
2021-05-31 15:20:23 -0700

    uses an env file to hold the API key

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import SearchBar from "./components/SearchBar"
 import axios from 'axios'
 import React, { useState } from "react";
 
-
 function App() {
 
   // const defaultResults = [
@@ -26,10 +25,9 @@ function App() {
 
   function search(query) {
     //performSearch(query);
-
     axios.get("https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search", {
       headers: {
-        Authorization: "Bearer "
+        Authorization: "Bearer " + process.env.REACT_APP_API_KEY
     },
       params: {
         "location": "94122",
